0275b4bfd8522d37a5a20bd1bc256fde828431a4
earnerswave <earnerswave@gmail.com>
2025-11-20 16:30:34 +0530

    feat: Add YouTube channels backend logic and Sequelize models

--- models/youtubeChannel.model.js
+++ models/youtubeChannel.model.js
@@ -1,32 +1,33 @@
 const { DataTypes } = require('sequelize');
 
+// Youtube Channel Table (Admin defined sections)
 module.exports = (sequelize) => {
-  const YoutubeChannel = sequelize.define(
-    'YoutubeChannel',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      channelName: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true, // सुनिश्चित करें कि चैनल का नाम यूनिक हो
-        field: 'channel_name',
-      },
-      channelLogoUrl: {
-        type: DataTypes.STRING(500),
-        allowNull: false,
-        field: 'channel_logo_url',
-      },
-    },
-    {
-      tableName: 'youtube_channels',
-      timestamps: true, // createdAt and updatedAt
-      underscored: true,
-      indexes: [{ fields: ['channel_name'] }],
-    }
-  );
-  return YoutubeChannel;
+  const YoutubeChannel = sequelize.define(
+    'YoutubeChannel',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      channelName: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true, 
+        field: 'channel_name',
+      },
+      channelLogoUrl: {
+        type: DataTypes.STRING(500),
+        allowNull: false,
+        field: 'channel_logo_url',
+      },
+    },
+    {
+      tableName: 'youtube_channels',
+      timestamps: true, 
+      underscored: true,
+      indexes: [{ fields: ['channel_name'] }],
+    }
+  );
+  return YoutubeChannel;
 };
